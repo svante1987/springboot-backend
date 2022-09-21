ccbc937c53e18e3b35ce2c1b2d524b80fcd9c180
Svante Pålsson <svantepalsson1987@gmail.com>
2022-09-21 04:44:02 +0200

    Both user and role models created and creating tables in database

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -2,13 +2,19 @@ package com.example.demo.model;
 
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import javax.persistence.ManyToMany;
+
 
 @Entity
 @Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
@@ -20,12 +26,24 @@ public class User {
 	
 	@Column(name = "first_name")
 	private String firstName;
+	
 	@Column(name="last_name")
 	private String lastName;
-	@Column(name="e-mail")
+	
 	private String email;
+	
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(
+					name = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(
+					name = "role_id", referencedColumnName = "id"))
+	
 	private Collection<Role> roles;
+	
 	
 	public User(String firstName, String lastName, String email, String password, Collection<Role> roles) {
 		super();
